8e8f3f23f25583b7590647804f892d6ee5554418
LauraSirumapea <lauraawise07@gmail.com>
2024-09-27 11:01:11 +0700

    Pair12

--- T02.java
+++ T02.java
@@ -1,1 +1,44 @@
-// NIM - Your Name
+// 12S24057 - Laura Awise Sirumapea
+// 12S24017 - Arya Pratama Sinambela
+
+import java.util.*;
+import java.lang.Math;
+
+public class T02{
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String isbn, judulbuku, penulisbuku, formatbuku, kategoribuku, penerbit;
+        double tahunterbit, hargabuku, minimummargin, stok, rating;
+
+        isbn = input.nextLine();
+        judulbuku = input.nextLine();
+        penulisbuku = input.nextLine();
+        tahunterbit = Double.parseDouble(input.nextLine());
+        penerbit = input.nextLine();
+        formatbuku = input.nextLine();
+        hargabuku = Double.parseDouble(input.nextLine());
+        minimummargin = Double.parseDouble(input.nextLine());
+        stok = Double.parseDouble(input.nextLine());
+        rating = Double.parseDouble(input.nextLine());
+        if (rating >= 4.7) {
+            kategoribuku = "best pick";
+        } else {
+            if (rating >= 4.5) {
+                kategoribuku = "must read";
+            } else {
+                if (rating >= 4.0) {
+                    kategoribuku = "recommended";
+                } else {
+                    if (rating >= 3.0) {
+                        kategoribuku = "average";
+                    } else {
+                        kategoribuku = "low";
+                    }
+                }
+            }
+        }
+        System.out.println(isbn + "|" + judulbuku + "|" + penulisbuku + "|" + tahunterbit + "|" + penerbit + "|" + formatbuku + "|" + hargabuku + "|" + minimummargin + "|" + stok + "|" + rating + "|" + kategoribuku);
+    }
+}
+
